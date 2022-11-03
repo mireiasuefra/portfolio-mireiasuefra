c93bb16f78f95f8e6e1479b02f6d2a848ab1e0ca
mireiasuefra <mireia.suefra@gmail.com>
2022-11-03 20:59:07 +0100

    json proyect created

---
+++ portfolio/src/components/proyect-list/ProyectList.tsx
@@ -0,0 +1,18 @@
+import "./ProyectList.scss";
+import React from "react";
+
+
+interface ProyectListProps {
+
+  }
+  
+  function ProyectList(props: ProyectListProps) {
+    return (
+      <div>    
+
+      </div>
+    );
+  }
+  
+  export default ProyectList;
+  
