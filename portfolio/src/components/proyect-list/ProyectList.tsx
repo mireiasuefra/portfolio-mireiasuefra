75fff72932c0ab21d546c2ea2325d62381bf4b05
mireiasuefra <mireia.suefra@gmail.com>
2022-11-03 21:39:41 +0100

    add styles

--- portfolio/src/components/proyect-list/ProyectList.tsx
+++ portfolio/src/components/proyect-list/ProyectList.tsx
@@ -2,31 +2,22 @@ import "./ProyectList.scss";
 import React from "react";
 import Footer from "../footer/Footer";
 
+interface ProyectListProps {}
 
-interface ProyectListProps {
-
-  }
-  
-
-
-
-  function ProyectList(props: ProyectListProps) {
-    return (
-        <div>
+function ProyectList(props: ProyectListProps) {
+  return (
+    <div>
       <section className="list">
-            
-       <h3 className="list__title">My proyects:</h3>
-       <p className="list__text">Here you can see some of the things I have built</p>
+        <h3 className="list__title">My proyects:</h3>
+        <p className="list__text">
+          Here you can see some of the things I have built
+        </p>
 
-       <ul>aqui va la lista de proyectos </ul>
-      
-
-       
+        <ul>aqui va la lista de proyectos </ul>
       </section>
-      <Footer/>
-      </div>
-    );
-  }
-  
-  export default ProyectList;
-  
+      <Footer />
+    </div>
+  );
+}
+
+export default ProyectList;
