7edcd6b573ebc4f4a3b7c58f8453e5d77145fc9e
mireiasuefra <mireia.suefra@gmail.com>
2022-11-07 13:07:45 +0100

    add styles

--- portfolio/src/components/project-item/ProjectItem.tsx
+++ portfolio/src/components/project-item/ProjectItem.tsx
@@ -11,11 +11,36 @@ function ProjectItem(props: ProjectItemProps) {
     <section className="item">
       <h4 className="item__name">{props.project.name}</h4>
       <div className="item__container">
-      <img className="item__image" src={props.project.image} alt="img"></img>
-      <p className="item__description">{props.project.description}</p>
+        <img className="item__image" src={props.project.image} alt="img"></img>
+        <div>
+          <div className="item__container-description">
+            <div>
+            <p className="item__description">{props.project.description}</p>
+            <p className="item__tech">
+              Technologies: {props.project.tech.join(", ")}{" "}
+            </p>
+            </div>
+            <div className="item__links">
+              <a
+                className="item__github"
+                href={props.project.github_url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="icon fab fa-github-square"></i>
+              </a>
+              <a
+                className="item__github"
+                href={props.project.url}
+                target="_blank"
+                rel="noreferrer"
+              >
+                <i className="fas fa-link"></i>
+              </a>
+            </div>
+          </div>
+        </div>
       </div>
-
-
     </section>
   );
 }
